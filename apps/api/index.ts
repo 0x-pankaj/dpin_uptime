0b3e02542c58f2bd9c4185a9b1543bb87c2d270a
pankaj <0xpankaj@gmail.com>
2025-03-20 00:01:04 +0545

    added jwt

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -2,9 +2,11 @@ console.log("Hello via Bun!");
 import express from "express";
 import { authMiddleware } from "./middleware";
 import { prismaClient } from "db/client";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -14,11 +16,14 @@ app.post("/api/v1/website", authMiddleware, async (req, res) => {
   const userId = req.userId!;
   const { url } = req.body;
 
-  console.log(userId);
+  // console.log(userId);
   const data = await prismaClient.website.create({
     data: {
       userId,
       url,
+    },
+    include: {
+      ticks: true,
     },
   });
   res.json({
@@ -43,22 +48,24 @@ app.get("/api/v1/website/status", authMiddleware, async (req, res) => {
   res.json(data);
 });
 
-app.get("/api/v1/website", authMiddleware, (req, res) => {
+app.get("/api/v1/website", authMiddleware, async (req, res) => {
   const userId = req.userId!;
-  const data = prismaClient.website.findMany({
+  const data = await prismaClient.website.findMany({
     where: {
       userId,
       disabled: false,
     },
+    include: { ticks: true },
   });
+  // console.log(data);
   res.json(data);
 });
 
-app.delete("/api/v1/website/", authMiddleware, (req, res) => {
+app.delete("/api/v1/website/", authMiddleware, async (req, res) => {
   const websiteId = req.query.websiteId as string;
   const userId = req.userId!;
   //find and update to disaple the website true
-  const data = prismaClient.website.update({
+  const data = await prismaClient.website.update({
     where: {
       id: websiteId,
       userId,
--- apps/api/middleware.ts
+++ apps/api/middleware.ts
@@ -1,4 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_PUBLIC_KEY } from "./config";
 
 export function authMiddleware(
   req: Request,
@@ -7,6 +9,27 @@ export function authMiddleware(
 ) {
   const authHeader = req.headers["authorization"];
   req.userId = "1";
+  if (!authHeader) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (token !== "1") {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+  const decoded = jwt.verify(token, JWT_PUBLIC_KEY);
+  console.log("decoded: ", decoded);
+  if (!decoded || !decoded.sub) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  req.userId = decoded.sub as string;
 
   next();
 }
